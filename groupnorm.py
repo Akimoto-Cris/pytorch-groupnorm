7697d11eee62536191a91ac8feea19a6872624dc
Akimoto-Cris <29163135+Akimoto-Cris@users.noreply.github.com>
2019-03-10 14:36:27 +0800

    robust when C not divisible by G

--- groupnorm.py
+++ groupnorm.py
@@ -13,12 +13,22 @@ class GroupNorm(nn.Module):
     def forward(self, x):
         N,C,H,W = x.size()
         G = self.num_groups
-        assert C % G == 0
-
-        x = x.view(N,G,-1)
-        mean = x.mean(-1, keepdim=True)
-        var = x.var(-1, keepdim=True)
-
-        x = (x-mean) / (var+self.eps).sqrt()
+        
+        if C % G == 0
+            x = x.view(N,G,-1)
+            mean = x.mean(-1, keepdim=True)
+            var = x.var(-1, keepdim=True)
+            x = (x-mean) / (var+self.eps).sqrt()
+        else:
+            whole = x[:, : G * (C // G), :, :]
+            mod = x[:, G * (C // G) + 1:, :, :]
+            whole_mean = whole.mean(-1, keepdim=True)
+            whole_var = whole.var(-1, keepdim=True)
+            mod_mean = mod.mean(-1, keepdim=True)
+            mod_var = mod.var(-1, keepdim=True)
+            whole, mod = tuple(map(lambda x, mean, var: (x-mean) / (var+self.eps).sqrt(), 
+                                   [whole, mod], [whole_mean, mod_mean], [whole_var, mod_var]))
+            x = torch.cat((whole, mod), axis=1)
+        
         x = x.view(N,C,H,W)
         return x * self.weight + self.bias
